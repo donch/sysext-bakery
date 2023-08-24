330a1afe81bc7b678aef497478a52bdc2070a1ab
David Donchez <david.donchez@enix.fr>
2023-08-24 17:47:11 +0200

    ci: test build

--- create_zfs_sysext.sh
+++ create_zfs_sysext.sh
@@ -5,8 +5,8 @@ export ARCH="${ARCH-amd64}"
 SCRIPTFOLDER="$(dirname "$(readlink -f "$0")")"
 
 if [ $# -lt 2 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-  echo "Usage: $0 VERSION SYSEXTNAME"
-  echo "The script will download the Teleport release binaries (e.g., for v9.6.23) and create a sysext squashfs image with the name SYSEXTNAME.raw in the current folder."
+  echo "Usage: $0 VERSION SYSEXTNAME FLATCARVERSION"
+  echo "The script will build ZFS modules and tooling and create a sysext squashfs image with the name SYSEXTNAME.raw in the current folder."
   echo "A temporary directory named SYSEXTNAME in the current folder will be created and deleted again."
   echo "All files in the sysext image will be owned by root."
   echo "To use arm64 pass 'ARCH=arm64' as environment variable (current value is '${ARCH}')."
@@ -16,6 +16,7 @@ fi
 
 VERSION="$1"
 SYSEXTNAME="$2"
+FLATCARVERSION="$3"
 if [ "${ARCH}" = aarch64 ]; then
   ARCH=arm64
 fi
@@ -25,7 +26,7 @@ rm -f zfs
 emerge-gitclone
 echo 'FEATURES="-network-sandbox -pid-sandbox -ipc-sandbox -usersandbox -sandbox"' >>/etc/portage/make.conf
 cp files/zfs/repos.conf /etc/portage/repos.conf/zfs.conf
-cp -r files/zfs/overlay/ /var/lib/portage/zfs-overlay/
+cp -r files/zfs/${FLATCARVERSION}/overlay/ /var/lib/portage/zfs-overlay/
 kernel=$(ls /lib/modules) && KBUILD_OUTPUT=/lib/modules/${kernel}/build KERNEL_DIR=/lib/modules/${kernel}/source emerge -j16 --getbinpkg --onlydeps zfs
 emerge -j16 --getbinpkg --buildpkgonly zfs squashfs-tools
 cat /lib/modules/5.15.122-flatcar/modules.dep
